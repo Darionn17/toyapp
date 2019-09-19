974eba93a8bb400dde4d99146531b9cbe8039e35
darionn17 <darionn17@gmail.com>
2019-09-19 20:47:59 +0100

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
-Class ApplicationController < ActionController::Base
-
-  def hello
-    render html: "hello, world!"
-  end
-end
+class ApplicationController < ActionController::Base
+    def hello
+        render html: "hello, world!"
+      end
+    end
