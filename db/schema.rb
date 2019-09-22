dc269a7b2d3fb07e992c7b06a91ac8a0d35eb7f8
darionn17 <darionn17@gmail.com>
2019-09-22 13:30:27 +0100

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_09_19_204633) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
